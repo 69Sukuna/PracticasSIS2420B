f4139a2afc313ce1b9285469b63141de57f1416b
69Sukuna <jose.garcia@sistemas.edu.bo>
2023-04-16 16:09:29 -0400

    P1 practico p2

---
+++ Practica1/Pregunta_2/Pregunta_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("Ingrese un numero: ");
+            n= Convert.ToInt32(Console.ReadLine());
+            Series.S1(n);
+            Series.S2(n);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practica1/Pregunta_2/Pregunta_2/Series.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta_2
+{
+    static class Series
+    {
+        public static void S1(int n)
+        {
+            Console.Write("\nSerie 1: ");
+            for(int i = 1, c = 1; i <= n; i++)
+            {
+                c *= i;
+                Console.Write(" , "+c);
+            }
+        }
+
+        public static void S2(int n)
+        {
+            double c = 0;
+            Console.Write("\nSerie 2: ");
+            for(int i = 1; i <= n; i++)
+            {
+                c = Math.Pow(2 , i);
+                Console.Write(" , "+c);
+            }
+        }
+    }
+}
